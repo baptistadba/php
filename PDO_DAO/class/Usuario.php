67dff7b66b90a73291eb030e393f566be72a449a
baptistadba <baptista.dba@gmail.com>
2018-08-05 10:43:39 -0300

    Aula de busca de listas

--- PDO_DAO/class/Usuario.php
+++ PDO_DAO/class/Usuario.php
@@ -43,7 +43,7 @@ class Usuario{
 		$this->dtcadastro=$value;		
 	}
 
-
+	//funçao carrega 1 usuario
 	public function loadById($id){
 		$sql = new SQL;
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(
@@ -63,6 +63,57 @@ class Usuario{
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));		
 
 		}
+
+	}
+
+	//funcao getList
+	public static function getList(){
+		
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	}
+
+	//busca por login
+	public static function search($login){
+
+		$sql =  new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios where deslogin like :SEARCH ORDER BY deslogin", array(
+						':SEARCH'=>"%".$login."%"
+		));
+
+	}
+
+
+	//funcao busca dados apos efetuar login usando a senha correta.
+	public function login($login,$senha){
+		$sql = new SQL;
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA",array(
+								":LOGIN"=>$login,
+								":SENHA"=>$senha
+
+		));
+
+
+		//if (isset($results[0])){
+		if (count($results) > 0){
+
+			$row =$results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));		
+
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos.", 1);
+			
+
+		}
+
 
 	}
 
--- PDO_DAO/index.php
+++ PDO_DAO/index.php
@@ -5,9 +5,23 @@ require_once("config.php");
 //$usuarios = $sql->select(" SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
-$root= new Usuario();
-$root->loadById(8);
-echo $root;
+
+//Carrega apenas um usuario
+//$root= new Usuario();
+//$root->loadById(8);
+//echo $root;
+
+//Carrega uma lista de usuarios(statico)
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Busca por login(statico)
+//$busca  = Usuario::search("te");
+//echo json_encode($busca);
 
 
+//carrega um usuario apos logar no sistema com login e senha corretas.
+$usuario = new Usuario();
+$usuario->login("usertest","#$%%");
+echo $usuario;
  ?>
